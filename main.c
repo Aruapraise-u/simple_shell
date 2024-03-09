c7ab518bc1c351eaba4acd09f12de58009baa5c9
Aruapraise-u <aruaobinna46@gmail.com>
2024-03-09 13:40:38 -0800

    0x16. C - Simple Shell

---
+++ main.c
@@ -0,0 +1,93 @@
+#include "shell.h"
+
+/**
+* my_prompt - function displays my prompt to terminal
+* Return: void
+*/
+void my_prompt(void)
+{
+	if (isatty(STDIN_FILENO))
+		shecktar_write("$ ");
+}
+
+/**
+* trim_spaces - leading and trailing spaces
+* @str: string to trim
+* Return: trimmed string
+*/
+char *trim_spaces(char *str)
+{
+	char *end;
+	/* Remove leading spaces */
+	while (*str != '\0' && sj_isspace((unsigned char)*str))
+	{
+		str++;
+	}
+
+    /* Remove trailing spaces */
+	end = str + my_strlen(str) - 1;
+
+	while (end > str && sj_isspace((unsigned char)*end))
+	{
+		end--;
+	}
+
+    /* Write new null terminator */
+	*(end + 1) = '\0';
+
+	return (str);
+}
+
+/**
+* main - Entry point
+*
+* Return: 0 success
+*/
+int main(void)
+{
+	char input[400];
+	char *result = NULL;
+	char **commands;
+
+	while (1)
+	{
+		char *trimmed_input;
+
+		my_prompt();
+		result = shecktar_getline(input, sizeof(input), stdin);
+		if (result == NULL)
+			break;
+
+		/* Trim leading and trailing spaces */
+		trimmed_input = trim_spaces(input);
+
+		/* we remove new line character */
+		trimmed_input[shecktar_strcspn(trimmed_input, "\n")] = '\0';
+
+		if (sj_strcmp(trimmed_input, "exit") == 0)
+			break;
+
+		if (sj_strcmp(trimmed_input, "env") == 0)
+		{
+			hndl_env();
+			continue;
+		}
+
+		commands = split(trimmed_input, ";");
+
+		hndl_mul_cmd(commands);
+		free(commands);
+	}
+	return (0);
+}
+
+/**
+* sj_isspace - function removes trailing spaces from code
+* @c: trailing characters
+* Return: 0 success
+*/
+int sj_isspace(int c)
+{
+	return (c == ' ' || c == '\t' || c == '\n' ||
+	c == '\v' || c == '\f' || c == '\r');
+}
---
+++ my_getline.c
@@ -0,0 +1,52 @@
+#include "shell.h"
+#define READ_SIZE 1024
+
+/**
+ * shecktar_getline - function reads the entire line from the file stream
+ * @buffer: buffer in which to store read chars
+ * @size: buffer size
+ * @stream: file stream from which to read
+ * Return: buffer on success, Null on failure or EOF
+ */
+char *shecktar_getline(char *buffer, size_t size, FILE *stream)
+{
+	static char internalBuffer[READ_SIZE];
+	static char *ptr;
+	static int charsLeft;
+	size_t i = 0;
+	char *comment_srt;
+
+	if (!buffer || size <= 0 || !stream)
+		return (NULL);
+
+	while (i < size - 1)
+	{
+		if (charsLeft <= 0)
+		{
+			charsLeft = read(fileno(stream), internalBuffer, READ_SIZE);
+			if (charsLeft <= 0)
+				break;
+			ptr = internalBuffer;
+		}
+		buffer[i++] = *ptr++;
+		charsLeft--;
+
+		if (buffer[i - 1] == '\n')
+			break;
+
+	}
+	if (i == 0)
+		return (NULL);
+
+	buffer[i] = '\0';
+
+	comment_srt = strchr(buffer, '#');
+
+	if (comment_srt != NULL && (comment_srt == buffer ||
+		*(comment_srt - 1) == ' '))
+	{
+		*comment_srt = '\0';
+	}
+
+	return (buffer);
+}
